78990799fa03e5e72f935bfdcc6dcb75175fd32c
vixen256 <64546358+vixen256@users.noreply.github.com>
2026-01-19 13:06:56 +1300

    Fix -fno-exceptions

--- prj/shared_ptr.hpp
+++ prj/shared_ptr.hpp
@@ -255,7 +255,7 @@ namespace prj {
     private:
         template<class U>
         void reset_ptr(U* ptr) {
-#if defined(__wasm__)
+#if defined(__wasm__) || !__cpp_exceptions
             // No exceptions
             this->reset_base(ptr, new ref_count<U>(ptr));
 #else
